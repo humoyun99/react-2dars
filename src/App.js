862f50995e67f37187f9dbc6c70eadf16f2bd255
Humoyin <baxranoofxumoyun@gmail.com>
2024-04-11 21:47:41 +0500

    nima

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import Title from './componut/Title';
 
 function App() {
   const [name,setname]=useState('ahror')
@@ -10,6 +11,7 @@ function App() {
     {title:"match :mineUnitidy vs barselone",id:3}
 
   ])
+  const [showContent,setShowContent]=useState(true)
   
   
   const handleClick=()=>{
@@ -21,19 +23,32 @@ function App() {
     const filteredEvents=events.filter((even)=>{
       return even.id !==id
     })
+    setEvents(filteredEvents)
   }
+
+
+
   return (
+    
     <div className="App">
+      <Title/>
       <h1>my name is {name} </h1>
       <p>yana nimalrdir</p>
       <button onClick={handleClick}>Change name</button>
-      {events.map((even)=>{
+      <br></br>
+      <hr></hr>
+      {showContent && <button onClick={()=>setShowContent(false)}>Hide conent</button>}
+      {!showContent && <button onClick={()=>setShowContent(true)}>show conent</button>}
+      {showContent && <div>
+        {events.length===0 && <h3>not comit yeet</h3>}
+        {events.map((even)=>{
         return(
           <div key={even.id}>
             <h2>{even.title}</h2>
             <button onClick={()=>handleDelete(even.id)}>Delate</button>
           </div>)
-      })
+      })}
+        </div>
       }
     </div>
   );
---
+++ src/componut/Title.js
@@ -0,0 +1,11 @@
+const Title=()=>{
+    return(
+        <div>
+            <h1 className="title">Humoyun kingdom events</h1>
+            <br></br>
+            <p className="subtitle">All events well be here</p>
+        </div>
+    )
+
+}
+export default Title
